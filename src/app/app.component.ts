258dfe70e71d606d9950097ceb64b502f9f43bec
aslamsk <aslamsk777@gmail.com>
2020-10-20 23:50:56 -0700

    bx-slider

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+declare var jquery:any;
+declare var $ :any;
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent  {
   title = 'testbxslider';
+
+  // ngOnInit() {
+  //   $(document).ready(function() {
+  //     $('.bxslider').bxSlider({
+  //       auto:true
+  //     });
+  // });
+  // }
+  ngAfterViewInit() {
+    $(document).ready(function(){
+      $('.bxslider').bxSlider({
+        slideWidth:450,
+        auto: true
+      }
+       
+      );
+    });
+  }
 }
